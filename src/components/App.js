cde2be734b93a204b5ffcd89953c7557f5e870d1
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-05-26 20:00:40 -0300

    feat: add component Post.
    solves task card #F0004.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Post from "./Post";
 export default class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <Post />
+      </div>
+    );
   }
 }
---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Post extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Post</h2>
+      </div>
+    );
+  }
+}
