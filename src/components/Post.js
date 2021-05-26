de360f47ec1387409c16916310ec75ad87cfed4f
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-05-26 20:15:27 -0300

    feat: add state in the Post.
    solves task card #F0004.

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,11 +3,20 @@ import React from "react";
 import Comment from "./Comment";
 
 export default class Post extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      comments: [{ text: "Bom post" }],
+    };
+  }
   render() {
     return (
       <div>
         <h2>{this.props.title}</h2>
-        <Comment text="Bom post" />
+        {this.state.comments.map((comment, index) => {
+          return <Comment key={index} text={comment.text} />;
+        })}
       </div>
     );
   }
