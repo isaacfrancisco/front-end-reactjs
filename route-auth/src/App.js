92686fbb236bb2c91d9445db0fee4aec0421fb5f
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-05-29 19:06:44 -0300

    feat: add file for creating routes.
    solves task card #F0004.

--- route-auth/src/App.js
+++ route-auth/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 
-class App extends Component {
-  render() {
-    return <div className="App"></div>;
-  }
-}
+import Routes from "./routes";
+
+const App = () => <Routes />;
 
 export default App;
