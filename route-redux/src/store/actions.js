f5532bbbae04510f1bb48cb0a806bc4c2aafdd34
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-05-30 19:35:40 -0300

    feat: add "history" and "index.js" to configure routes.
    solves task card #F0004.

--- route-redux/src/store/actions.js
+++ route-redux/src/store/actions.js
@@ -1,3 +1,10 @@
-export const signIn = () => dispatch => {
-  // Do some stuff to authenticate
+import { push } from "connected-react-router";
+
+export const signIn = () => (dispatch) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Go!");
+      dispatch(push("/profile"));
+    }, 3000);
+  });
 };
--- route-redux/src/store/index.js
+++ route-redux/src/store/index.js
@@ -1,6 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { connectRouter, routerMiddleware } from "connected-react-router";
 
-const store = createStore(() => {}, applyMiddleware(thunk));
+import history from "../routes/history";
+
+const middlewares = [thunk, routerMiddleware(history)];
+
+const store = createStore(
+  connectRouter(history)(() => {}),
+  applyMiddleware(...middlewares)
+);
 
 export default store;
