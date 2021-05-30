43871f13c5b8b5a7319cdeaa9a38cbb1c7634b49
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-05-30 09:14:37 -0300

    chore: customize import project path.
    solves task card #F0004.

--- path-cra/src/App.js
+++ path-cra/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+import TodoItem from "./components/TodoList/TodoItem";
 class App extends Component {
   render() {
     return <div className="App"></div>;
