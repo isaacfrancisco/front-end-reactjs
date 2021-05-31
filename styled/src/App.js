7132bcfda21fddd2a65ab05892e33ad86e5b8282
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-05-31 15:41:44 -0300

    feat: create "Title" component.
    solves task card #F0004.

--- styled/src/App.js
+++ styled/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 
+import { Title } from "./styles";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
+      <div>
+        <Title>Hello World</Title>
       </div>
     );
   }
