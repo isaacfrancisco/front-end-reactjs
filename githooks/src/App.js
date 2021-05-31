cb9a778084a2bf3714b63bbcb153692f2c28fe0b
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-05-31 17:31:54 -0300

    feat: add button to favorite repository.
    solves task card #F0004.

--- githooks/src/App.js
+++ githooks/src/App.js
@@ -3,20 +3,34 @@ import React, { useState, useEffect } from "react";
 export default function App() {
   const [repositories, setRepositories] = useState([]);
 
-  useEffect(async () => {
-    const response = await fetch(
-      "https://api.github.com/users/isaacfrancisco/repos"
-    );
-    const data = await response.json();
-
-    setRepositories(data);
+  useEffect(() => {
+    async function getRepositories() {
+      const response = await fetch(
+        "https://api.github.com/users/isaacfrancisco/repos"
+      );
+      const data = await response.json();
+      setRepositories(data);
+    }
+    getRepositories();
   }, []);
+
+  const handleFavorite = (id) => {
+    const newRepositories = repositories.map((repo) => {
+      return repo.id === id ? { ...repo, favorite: true } : repo;
+    });
+
+    setRepositories(newRepositories);
+  };
 
   return (
     <>
       <ul>
         {repositories.map((repo) => (
-          <li key={repo.id}>{repo.name}</li>
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span>- Favorito</span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
         ))}
       </ul>
     </>
