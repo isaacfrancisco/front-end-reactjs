1ab446781c4d6aa1b108920d6d59076a54085959
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-05-31 13:12:33 -0300

    feat: listing urls by api.
    a component and function was created that scrolls through a list to show the urls.
    
    solves task card #F0004.

--- ssr-next/pages/index.js
+++ ssr-next/pages/index.js
@@ -1,3 +1,19 @@
 import React from "react";
+import "isomorphic-fetch";
 
-export default () => <h1>Hello World</h1>;
+const Home = ({ repositories }) => (
+  <div>
+    {repositories.map((repo) => (
+      <h1 key={repo.id}>{repo.name}</h1>
+    ))}
+  </div>
+);
+
+Home.getInitialProps = async () => {
+  const response = await fetch("https://api.github.com/orgs/rocketseat/repos");
+  const repositories = await response.json();
+
+  return { repositories };
+};
+
+export default Home;
