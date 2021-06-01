69785a8b1764d72e4b935fc40e57faeb7bba680e
Isaac Francisco <isaac-silvajf@hotmail.com>
2021-06-01 08:11:37 -0300

    chore: update packages and configuring eslint.
    solves task card #F0004.

--- tsreact/src/App.tsx
+++ tsreact/src/App.tsx
@@ -1,23 +1,13 @@
-import React from 'react';
+import React, { Component }  from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    console.log("aaa")
+  }
+
+  render() {
+    return <h1>Hello World</h1>;
+  }
 }
 
 export default App;
